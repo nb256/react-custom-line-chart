d39602f33835d44f6d9e37a98d6345880ef1c6a1
Naim Buru <nbur4u@gmail.com>
2019-07-25 19:13:50 +0300

    main container to contain graph component

---
+++ src/containers/Main.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getGraphData } from "../actions";
+import Graph from "../components/Graph";
+import { maxValueSelector } from "../selectors/graphSelector";
+import loading from "../media/loading.svg";
+
+function Main(props) {
+  useEffect(() => {
+    props.getGraphData();
+  }, []);
+  return (
+    <div style={{ position: "relative" }}>
+      {props.graphReducer.loading && (
+        <div
+          style={{
+            position: "absolute",
+            left: "44%",
+            top: "38%",
+            zIndex: "100",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center"
+          }}
+        >
+          <img
+            src={loading}
+            style={{ marginRight: "10px", width: "40px", height: "40px" }}
+          />
+          Loading
+        </div>
+      )}
+      <Graph
+        data={props.graphReducer.data}
+        maxValue={maxValueSelector(props.graphReducer)}
+      />
+    </div>
+  );
+}
+
+export default connect(
+  state => state,
+  {
+    getGraphData
+  }
+)(Main);
