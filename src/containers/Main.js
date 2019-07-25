1a60c87092d905fa972bdff8b122be2dff439d7d
Naim Buru <nbur4u@gmail.com>
2019-07-25 19:45:16 +0300

    extract data string from relative container

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,33 +10,35 @@ function Main(props) {
     props.getGraphData();
   }, []);
   return (
-    <div style={{ position: "relative" }}>
-      {props.graphReducer.loading && (
-        <div
-          style={{
-            position: "absolute",
-            left: "44%",
-            top: "38%",
-            zIndex: "100",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center"
-          }}
-        >
-          <img
-            src={loading}
-            style={{ marginRight: "10px", width: "40px", height: "40px" }}
-          />
-          Loading
-        </div>
-      )}
-      <Graph
-        data={props.graphReducer.data}
-        maxValue={maxValueSelector(props.graphReducer)}
-      />
+    <>
+      <div style={{ position: "relative" }}>
+        {props.graphReducer.loading && (
+          <div
+            style={{
+              position: "absolute",
+              left: "44%",
+              top: "38%",
+              zIndex: "100",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center"
+            }}
+          >
+            <img
+              src={loading}
+              style={{ marginRight: "10px", width: "40px", height: "40px" }}
+            />
+            Loading
+          </div>
+        )}
+        <Graph
+          data={props.graphReducer.data}
+          maxValue={maxValueSelector(props.graphReducer)}
+        />
+      </div>
       <h2>Data</h2>
       <code>{JSON.stringify(props.graphReducer.data)}</code>
-    </div>
+    </>
   );
 }
 
